6c668a73b571b1001cff5a2f4258904bc3a95751
Howard Gil <howardbgil@gmail.com>
2024-07-24 16:11:21 -0700

    Now overriding prompt

--- agentops/llm_tracker.py
+++ agentops/llm_tracker.py
@@ -444,7 +444,10 @@ class LlmTracker:
 
             prompt_override = fetch_prompt_override_from_time_travel_cache(kwargs)
             if prompt_override:
-                kwargs["messages"] = prompt_override  # TODO: FIX
+                import json
+
+                prompt_override = json.loads(prompt_override)
+                kwargs["messages"] = prompt_override["messages"]
 
             # Call the original function with its original arguments
             result = original_create(*args, **kwargs)
