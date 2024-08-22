de5ce97dedaac815976e1eef1ba2e6fd0fae9de2
Braelyn Boynton <bboynton97@gmail.com>
2024-08-21 17:38:04 -0700

    manual tests for hosted crew

---
+++ tests/core_manual_tests/api_server_crew/demo_crew.py
@@ -0,0 +1,69 @@
+import os
+
+from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
+import agentops
+from fastapi import FastAPI
+from dotenv import load_dotenv
+from openai import OpenAI
+from crewai import Agent, Crew, Task
+from langchain.globals import set_debug
+
+# set_debug(True)
+load_dotenv()
+
+agentops.init(auto_start_session=False)
+openai = OpenAI()
+session = agentops.start_session(tags=["api-server-test"])
+
+# llm = ChatOpenAI(model_name="gpt-4", temperature=0.7)
+
+llm = ChatGroq(
+    temperature=0,
+    groq_api_key=os.environ.get("GROQ_API_KEY"),
+    model_name="llama3-70b-8192",
+)
+
+agent = Agent(
+    role="greeter",
+    goal="make sure every person feels welcomed",
+    backstory="you partied too much in school and now you're trying to turn your life around. you applied for a "
+    "job at walmart as a greeter and you want to do as well as possible at greeting as your first rung "
+    "on the climb up the corporate ladder.",
+    verbose=False,
+    llm=llm,
+)
+
+tasks = []
+task1 = Task(
+    description="a man in a blue coat walks through the door",
+    agent=agent,
+    expected_output="a script of what the greeter will say",
+)
+tasks.append(task1)
+
+task2 = Task(
+    description="a woman with a small dog in her arms rushes through the door, seemingly in a hurry",
+    agent=agent,
+    expected_output="a script of what the greeter will say",
+)
+tasks.append(task2)
+
+task3 = Task(
+    description="a tall, grizzled looking man with a sour look on his face enters the store. he appears to be in a bad "
+    "mood.",
+    agent=agent,
+    expected_output="a script of what the greeter will say",
+)
+tasks.append(task3)
+
+crew = Crew(agents=[agent], tasks=tasks, verbose=True)
+
+# Crew Execution
+output = crew.kickoff()
+
+# End session
+
+session.end_session(end_state="Success")
+
+print(output)
---
+++ tests/core_manual_tests/api_server_crew/readme.md
@@ -0,0 +1,16 @@
+# API server test with Crew
+This is a manual test with two files. It checks to make sure that the SDK works in an API environment.
+
+This test was created to run Crew in a server environment for the purpose of testing Crew in multi-session scenarios.
+
+## Running
+1. `python server.py`
+2. In different terminal, `python client.py`
+
+## Validate
+Check in your AgentOps Dashboard 
+
+1. two sessions are created with the `api-server-test` tag.
+2. Each session should have one `LLMEvent` and one `ActionEvent`.
+3. Both sessions should have an end state of `Success`
+4. Neither session should have multiple agents
